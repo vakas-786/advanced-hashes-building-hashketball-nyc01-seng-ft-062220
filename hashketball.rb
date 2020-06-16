4b073665712609225d8cef0114576707d4e0023a
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-16 22:59:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,38 +15,36 @@ def game_hash
         steals: 3,
         blocks: 1,
         slam_dunks: 1 },
-        binding.pry
-        puts "Hello"
         {
           player_name: "Reggie Evans",
-          number: "30",
-          shoe: "14",
-          points: "12",
-          rebounds: "12",
-          assists: "12",
-          steals: "12",
-          blocks: "12",
-          slam_dunks: "7" },
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7 },
           {
             player_name: "Brook Lopez",
-            number: "11",
-            shoe: "17",
-            points: "17",
-            rebounds: "19",
-            assists: "10",
-            steals: "3",
-            blocks: "1"
-            slam_dunks: "15"
+            number: 11,
+            shoe: 17,
+            points: 17,
+            rebounds: 19,
+            assists: 10,
+            steals: 3,
+            blocks: 1
+            slam_dunks: 15
             {
               player_name: "Mason Plumlee"
-              number: "1",
-              shoe: "19",
-              points: "26",
-              rebounds: "11",
-              assists: "6",
-              steals: "3",
-              blocks: "8"
-              slam_dunks:"5" }
+              number: 1,
+              shoe: 19,
+              points: 26,
+              rebounds: 11,
+              assists: 6,
+              steals: 3,
+              blocks: 8
+              slam_dunks: 5 }
               {
                 player_name: "Jason Terry"
                 number: "31",
