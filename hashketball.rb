ebef63b7c080afa4cdf44b1406ca66e89d491272
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-16 22:55:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,7 +13,7 @@ def game_hash
         assists: "12",
         steals: "3",
         blocks: "1",
-        slam_dunks: "1" },
+        slam_dunks: 1 },
         {
           player_name: "Reggie Evans",
           number: "30",
