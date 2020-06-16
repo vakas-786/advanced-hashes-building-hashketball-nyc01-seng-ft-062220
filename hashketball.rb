c6f5f2056aa7c7f524ab29a2babfd71df162b2ff
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-16 23:05:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,8 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{
+      :players => [
+      {
         :player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
