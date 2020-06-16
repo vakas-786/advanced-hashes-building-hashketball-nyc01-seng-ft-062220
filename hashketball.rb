36607f94bcb49069de5336640ee299b6ae725528
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-16 23:04:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -98,7 +98,9 @@ def game_hash
               :assists => 12,
               :steals => 7,
               :blocks => 5,
-              :slam_dunks => 12 }]
+              :slam_dunks => 12
+            }
+          ]
     }
   }
 end
