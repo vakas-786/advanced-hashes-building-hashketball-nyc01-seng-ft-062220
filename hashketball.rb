4dfc953f32e9f477927f6522089cf2553eadf64f
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-16 23:00:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -50,11 +50,11 @@ def game_hash
                 number: 31,
                 shoe: 15,
                 points: 19,
-                rebounds: "2",
-                assists: "2",
-                steals: "4",
-                blocks: "11"
-                slam_dunks: "1" }]
+                rebounds: 2,
+                assists: 2,
+                steals: 4,
+                blocks: 11,
+                slam_dunks: 1 }]
           },
           :away => {
             :team_name => "Charlotte Hornets",
