6ccfedbe6407258b99a2f1282461383d843b53cb
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-16 22:46:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,12 +36,14 @@ def game_hash
             slam_dunks: "15"
             {
               player_name: "Mason Plumlee"
-              number: "",
-              shoe: "",
-              points: "",
-              rebounds: "",
-              assists: "",
-              steals: "",}
+              number: "1",
+              shoe: "19",
+              points: "26",
+              rebounds: "11",
+              assists: "6",
+              steals: "3",
+              blocks: "8"
+              slam_dunks:"5" }
               {
                 player_name: "Jason Terry"
                 number: "",
@@ -49,7 +51,9 @@ def game_hash
                 points: "",
                 rebounds: "",
                 assists: "",
-                steals: "",}
+                steals: "",
+                blocks: ""
+                slam_dunks: "" }
           }
       ]
 
